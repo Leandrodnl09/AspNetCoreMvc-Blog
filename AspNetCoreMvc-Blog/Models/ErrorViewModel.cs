813cf3bf0cb8cc89be2a42465724fc01447826ca
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-03-02 12:19:14 -0300

    Adicionar arquivos de projeto.

---
+++ AspNetCoreMvc-Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreMvc_Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
